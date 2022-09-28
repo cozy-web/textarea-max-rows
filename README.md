4d9a8cf8f376d2067addfb5c2193a2c7677a84a1
c4710n <c4710n@users.noreply.github.com>
2022-09-28 23:43:24 +0800

    feat: add working code

---
+++ README.md
@@ -0,0 +1,5 @@
+# textarea-max-rows
+
+> Expand a textarea to fit its content, between a minimum (`rows` attribute) and maximum (`max-rows` attribute) number of rows.
+
+An improved version of [`max-rows` attribute support of <textarea></textarea>](https://gist.github.com/hubgit/e08998bf2dfdec556b2726d13035cd2c).
---
+++ textarea-max-rows.js
@@ -0,0 +1,83 @@
+function createElement(tagName, attributes) {
+  const element = document.createElement(tagName)
+
+  for (const [key, value] of Object.entries(attributes)) {
+    if (value) {
+      element.setAttribute(key, value)
+    }
+  }
+
+  return element
+}
+
+function moveElementOffscreen(element) {
+  const move = () => {
+    element.style.position = 'absolute'
+    element.style.left = `-${document.body.clientWidth * 2}px`
+  }
+
+  move()
+  window.addEventListener('resize', move)
+
+  return element
+}
+
+function syncWidthFrom(newElement, existingElement) {
+  const resizeObserver = new ResizeObserver(([entry], _observer) => {
+    const width = entry.target.offsetWidth
+    newElement.style.width = `${width}px`
+  })
+
+  resizeObserver.observe(existingElement)
+  return resizeObserver
+}
+
+function insertAfter(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling)
+}
+
+function patchTextarea(element) {
+  const attrClass = element.getAttribute('class')
+  const attrStyle = element.getAttribute('style')
+  const attrRows = element.getAttribute('rows') || 1
+  const attrMaxRows = element.getAttribute('max-rows')
+
+  const minRows = Number.parseInt(attrRows)
+  const maxRows = Number.parseInt(attrMaxRows)
+
+  const shadowElement = createElement('textarea', {
+    class: attrClass,
+    style: `box-sizing: border-box; ${attrStyle}`,
+    rows: attrRows,
+    'max-rows': attrMaxRows,
+  })
+  moveElementOffscreen(shadowElement)
+  syncWidthFrom(shadowElement, element)
+  insertAfter(shadowElement, element)
+
+  element.addEventListener('input', function () {
+    // copy the content from the real textarea
+    shadowElement.value = element.value
+
+    // get the height of content
+    shadowElement.setAttribute('rows', 1)
+    const contentHeight = shadowElement.scrollHeight
+
+    // increase the number of rows until finding a proper number.
+    for (let rows = minRows; rows <= maxRows; rows++) {
+      shadowElement.setAttribute('rows', rows)
+
+      if (shadowElement.clientHeight >= contentHeight) {
+        break
+      }
+    }
+
+    element.setAttribute('rows', shadowElement.getAttribute('rows'))
+  })
+  element.dispatchEvent(new Event('input'))
+}
+
+window.addEventListener('load', () => {
+  const textareas = document.querySelectorAll('textarea[max-rows]')
+  Array.prototype.forEach.call(textareas, patchTextarea)
+})
